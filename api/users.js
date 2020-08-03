c60ab5b0be4f1adb3290889f669bffe174c8f445
agheadley <agheadley@gmail.com>
2020-08-03 07:56:28 +0100

    v01h

--- api/users.js
+++ api/users.js
@@ -1,21 +1,15 @@
-import { MongoClient } from "mongodb";
-module.exports = (req, res) => {
-  res.send("***" + process.env.VERCEL_URL);
-};
+//import { MongoClient } from "mongodb";
 
 /*
 module.exports = (req, res) => {
-  try {
-    mongoose.connect(
-      process.env.MONGODB_URI,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => console.log("connected")
-    );
-    res.send("connected" + process.env.MONGODB_URI);
-  } catch (error) {
-    console.log("could not connect via mongoose");
-    res.send("error in connection");
-  }
+  res.send("***" + process.env.VERCEL_URL);
 };
-
 */
+
+module.exports = (req, res) => {
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    if (err) res.send("error with mongoclient");
+    res.send("Database connected");
+    db.close();
+  });
+};
