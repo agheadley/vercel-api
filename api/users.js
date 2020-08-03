bea30bca61359af20ca8fecda927af56808bd5d8
agheadley <agheadley@gmail.com>
2020-08-03 08:20:08 +0100

    v01k

--- api/users.js
+++ api/users.js
@@ -1,15 +1,25 @@
 import { MongoClient } from "mongodb";
 
 /*
-module.exports = (req, res) => {
-  res.send("***" + process.env.VERCEL_URL);
-};
-*/
 
 module.exports = (req, res) => {
   MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
     if (err) res.send("error with mongoclient");
     res.send("Database connected");
     db.close();
+  });
+};
+
+*/
+
+module.exports = (req, res) => {
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("db0");
+    dbo.collection("c0").find({}, function(err, result) {
+      if (err) res.send("error with mongoclient");
+      res.send(result);
+      db.close();
+    });
   });
 };
